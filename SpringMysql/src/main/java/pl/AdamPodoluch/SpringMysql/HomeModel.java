ddeafbcafc6abc8842c856a3884bab4cc731c2e6
Thanders15 <48353618+Thanders15@users.noreply.github.com>
2021-04-27 14:30:09 +0200

    Utworzenie projektu + SecurityConfig + Model

---
+++ SpringMysql/src/main/java/pl/AdamPodoluch/SpringMysql/HomeModel.java
@@ -0,0 +1,21 @@
+package pl.AdamPodoluch.SpringMysql;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeModel {
+
+    @GetMapping("/")
+    public String home(){
+        return ("Welcome");
+    }
+    @GetMapping("/user")
+    public String user(){
+        return ("Welcome user");
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return ("Welcome admin");
+    }
+}
